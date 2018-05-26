8d95c86edd781c6f69f32188b84d1f2654a960d3
MikelG <mikel.garralaga@estudiant.upc.edu>
2018-05-26 20:38:45 +0200

    Update

--- Diffie_hellman/diffie-hellman.js
+++ Diffie_hellman/diffie-hellman.js
@@ -3,49 +3,6 @@
 const bigInt = require('big-integer');
 const crypto = require('crypto');
 
-const getAliceParameters = function (bitLength = 512) {
-    let n = bigInt(4);
-    const g = bigInt(2);
-    const bits = bigInt(512);
-
-    while (!n.isPrime()) {
-        n = bigInt.randBetween(bigInt(2).pow(bits.minus(1)), bigInt(2).pow(bits).minus(1))
-    }
-    // A genera
-    const a = bigInt(bigInt.randBetween(2, n.minus(1)));
-
-    // A envía
-    const A = bigInt(g.modPow(a, n));
-
-    let parameters = {};
-    parameters.n = n;
-    parameters.g = g;
-    parameters.a = a;
-    parameters.A = A;
-
-    return parameters;
-}
-
-const getBobParameters = function (n, g, A) {
-
-    const b = bigInt(bigInt.randBetween(2, n.minus(1)));
-    const B = bigInt(g.modPow(b, n));
-
-    let parameters = {};
-    parameters.n = n;
-    parameters.g = g;
-    parameters.b = b;
-    parameters.B = B;
-
-    return parameters;
-}
-
-const getDHKey = function (B, a, n) {
-
-    const dhKey = bigInt(B.modPow(a, n));
-    return dhKey;
-}
-
 bigInt.rand = function (bitLength) {
     let bytes = bitLength / 8;
     let buf = Buffer.alloc(bytes);
